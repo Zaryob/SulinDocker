332a65e00476ae8e43da7058e1f17ccf5583eee4
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-09-03 15:23:45 +0300

    Sulin Minimal image Dockerfile

---
+++ sulin_minimal/generate-tarfile.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set +h
+umask 022
+sudo inary ar core http://127.0.0.1/indispensable/inary-index.xml.xz -Dsoot
+sudo inary install acl attr bash bzip2 ca-certificates    \
+                   coreutils cracklib curl expat gdbm     \
+                   gettext glib2 glibc gmp libcap libffi  \
+                   libgcc libgomp libidn2 libpcre libssh2 \
+                   libunistring libxml2 ncurses openssl   \
+                   pam python3 python3-psutil             \
+                   python3-pycurl readline run-parts      \
+                   sqlite timezone unzip xz zlib --ignore-scom --ignore-safety -Dsoot
+
+cd soot
+tar cpvf ../sulin.tar *
+cd ..
+
