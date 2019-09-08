5f32ac1d96dfa4c85a614c015875007019779b00
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-09-08 15:09:39 +0300

    Regenerated Dockerfile's

--- sulin_minimal/generate-tarfile.sh
+++ sulin_minimal/generate-tarfile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set +h
 umask 022
-sudo inary ar core http://127.0.0.1/indispensable/inary-index.xml.xz -Dsoot --yes-all
+sudo inary ar core https://netix.dl.sourceforge.net/project/sulinos/SulinRepository/inary-index.xml.xz -Dsoot --yes-all
 sudo inary install acl attr bash bzip2 ca-certificates    \
                    coreutils cracklib curl expat gdbm     \
                    gettext glib2 glibc gmp libcap libffi  \
@@ -9,7 +9,7 @@ sudo inary install acl attr bash bzip2 ca-certificates    \
                    libunistring libxml2 ncurses openssl   \
                    pam python3 python3-psutil             \
                    python3-pycurl readline run-parts      \
-                   sqlite timezone unzip xz zlib --ignore-scom --ignore-safety -Dsoot --yes-all
+                   sqlite timezone unzip xz zlib baselayout --ignore-scom --ignore-safety -Dsoot --yes-all
 
 cd soot
 tar cpvf ../sulin.tar *
