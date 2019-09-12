e5e1fc22c14228ff0b4f60ac2ed48d9e19849917
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-09-12 05:56:33 +0300

    Hepsi tek pakete

--- sulin_minimal/generate-tarfile.sh
+++ sulin_minimal/generate-tarfile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set +h
 umask 022
-sudo inary ar core https://netix.dl.sourceforge.net/project/sulinos/SulinRepository/inary-index.xml.xz -Dsoot --yes-all
+sudo inary ar core https://master.dl.sourceforge.net/project/sulinos/SulinRepository/inary-index.xml.xz -Dsoot --yes-all
 sudo inary install acl attr bash bzip2 ca-certificates    \
                    coreutils cracklib curl expat gdbm     \
                    gettext glib2 glibc gmp libcap libffi  \
@@ -9,8 +9,7 @@ sudo inary install acl attr bash bzip2 ca-certificates    \
                    libunistring libxml2 ncurses openssl   \
                    pam python3 python3-psutil             \
                    python3-pycurl readline run-parts      \
-                   sqlite timezone unzip xz zlib baselayout --ignore-scom --ignore-safety -Dsoot --yes-all
+                   sqlite timezone unzip xz zlib baselayout scom --ignore-scom --ignore-safety -Dsoot --yes-all
 cd soot
 rm -rf var/cache/inary/packages
-tar cpvf ../sulin.tar *
 cd ..
