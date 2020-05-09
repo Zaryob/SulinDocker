003bce9a90eabb72a9b3cd4c9b84349bebaf87a6
Suleyman Poyraz <zaryob.dev@gmail.com>
2020-05-09 10:25:15 +0000

    inary yeni versiyona uygun düzenleme

--- sulin_minimal/generate-tarfile.sh
+++ sulin_minimal/generate-tarfile.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 set +h
 umask 022
-sudo inary ar core https://master.dl.sourceforge.net/project/sulinos/SulinRepository/inary-index.xml.xz -Dsoot --yes-all
-sudo inary install acl attr bash bzip2 ca-certificates    \
+inary-cli ar core https://master.dl.sourceforge.net/project/sulinos/SulinRepository/inary-index.xml -Dsoot --yes-all
+inary-cli install acl attr bash bzip2 ca-certificates    \
                    coreutils cracklib curl expat gdbm     \
                    gettext glib2 glibc gmp libcap libffi  \
                    libgcc libgomp libidn2 libpcre libssh2 \
                    libunistring libxml2 ncurses openssl   \
                    pam python3 python3-psutil             \
                    python3-pycurl readline run-parts      \
-                   sqlite timezone unzip xz zlib baselayout inary scom --ignore-scom --ignore-safety -Dsoot --yes-all
+                   sqlite timezone unzip xz zlib baselayout inary --ignore-conf --ignore-safety -Dsoot --yes-all
 cd soot
 sudo rm -rf var/cache/inary/packages
 cd ..
